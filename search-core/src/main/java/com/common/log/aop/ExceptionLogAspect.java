7458d265443e6d7994cc6dce562591734351da08
gangdodan <dodannnn4@gmail.com>
2023-07-04 20:16:23 +0900

    Feat: logging 구현

---
+++ search-core/src/main/java/com/common/log/aop/ExceptionLogAspect.java
@@ -0,0 +1,34 @@
+package com.common.log.aop;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * JoinPoint에서 Exception이 발생하면 AOP에서도 Exception을 Throw 해주어야 Handler에서 캐치할 수 있음
+ * */
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class ExceptionLogAspect {
+    private final Logger logger = LoggerFactory.getLogger(ExceptionLogAspect.class);
+
+    @AfterThrowing(pointcut = "within(com.example.*)", throwing = "exception")
+    public void logException(JoinPoint joinPoint, Throwable exception) throws Throwable {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        printErrorLog(exception, className, methodName);
+
+        throw exception;
+    }
+
+    private void printErrorLog(Throwable exception, String className, String methodName) {
+        logger.error("Exception in {}.{}() with cause = {}", className, methodName,
+                exception.getCause() != null ? exception.getCause() : "NULL");
+    }
+
+}
