4f612cf7d1a138a9e57b2f2bb5494e1f7f586f0d
gangdodan <dodannnn4@gmail.com>
2023-07-04 17:36:29 +0900

    Fix: Page Response 변경

--- search-internal-api/src/main/java/com/search/controller/SearchBlogController.java
+++ search-internal-api/src/main/java/com/search/controller/SearchBlogController.java
@@ -1,7 +1,10 @@
 package com.search.controller;
 
+import com.common.dto.PageResponseDto;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.search.application.SearchOpenApiService;
+import com.search.dto.TrendKeyword;
+import com.search.service.SearchTrendKeywordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,23 +12,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
-@RequestMapping("/search/blogs")
+@RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchBlogController {
     private final SearchOpenApiService openApiService;
+    private final SearchTrendKeywordService trendKeywordService;
 
-    @GetMapping
-    public Page<JsonNode> getSearchResultByKakao(
-            @RequestParam ("query") String query,
-            @RequestParam ("sort") String sort,
-            @RequestParam ("page") Integer page,
-            @RequestParam ("size") Integer size
+    @GetMapping("/blogs")
+    public PageResponseDto<JsonNode> getSearchResultByKakao(
+            @RequestParam(required = true,name = "query") String query,
+            @RequestParam(required = false,name ="sort") String sort,
+            @RequestParam(required = false,name ="page") Integer page,
+            @RequestParam(required = false,name ="size") Integer size
     ) {
-        String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(System.currentTimeMillis());
-        return openApiService.requestSearchResult(query, sort, page, size,timestamp);
+        Page<JsonNode> results = openApiService.requestSearchResult(query, sort, page, size);
+        return PageResponseDto.of(results);
+    }
+
+    @GetMapping("/keywords/popular")
+    public List<TrendKeyword> getSearchTrendKeyword() {
+        return trendKeywordService.getTrendKeywords(LocalDate.now());
     }
 
 }
