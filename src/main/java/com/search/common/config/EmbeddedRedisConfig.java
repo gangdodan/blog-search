555ee82561f3b1f9544bd52cfef336af2a8f829e
gangdodan <dodannnn4@gmail.com>
2023-07-05 22:29:57 +0900

    Fix: Embededd redis 설정

---
+++ src/main/java/com/search/common/config/EmbeddedRedisConfig.java
@@ -0,0 +1,62 @@
+package com.search.common.config;
+
+import io.micrometer.common.util.StringUtils;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Profile({"test", "local"})
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() throws IOException {
+        if(isRedisRunning()) return;
+
+        redisServer = new RedisServer(port);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(port));
+    }
+
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+        } catch (Exception e) {
+        }
+        return !StringUtils.isEmpty(pidInfo.toString());
+    }
+}
